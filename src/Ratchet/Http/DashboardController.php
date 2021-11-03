74717f1188ebad9e62e95952f537713799ea8716
peng1202 <31307972+peng1202@users.noreply.github.com>
2021-11-03 15:15:14 +0800

    Update DashboardController.php

--- src/Ratchet/Http/DashboardController.php
+++ src/Ratchet/Http/DashboardController.php
@@ -6,13 +6,14 @@ use GuzzleHttp\Psr7\Response;
 use function GuzzleHttp\Psr7\str;
 use Psr\Http\Message\RequestInterface;
 use Ratchet\ConnectionInterface;
+use GuzzleHttp\Psr7;
 
 class DashboardController extends Controller
 {
     public function onOpen(ConnectionInterface $connection, RequestInterface $request = null)
     {
         $connection->send(
-            str(new Response(
+            Psr7\Message::toString(new Response(
                 200,
                 ['Content-Type' => 'text/html'],
                 file_get_contents(__DIR__.'/../../../resources/views/index.html')
